a17ef790b0cfda79b3877e7b2a21b20895bffe2d
Eric-Paul Lecluse <e@epologee.com>
2013-03-21 13:21:23 +0100

    Added a fix for using nested contexts on iOS 5. It will block the main thread while saving, but it should not trigger the Core Data deadlock bug.

--- TwelveTwentyToolkit/Persistence/TTAbstractPersistenceProxy.m
+++ TwelveTwentyToolkit/Persistence/TTAbstractPersistenceProxy.m
@@ -1,35 +1,43 @@
+#import <TwelveTwentyToolkit/TwelveTwentyToolkit.h>
 #import "TTAbstractPersistenceProxy.h"
 #import "TTTLog.h"
+#import "TwelveTwentyToolkit.h"
 
 #define TT_PERSISTENCE_THRESHOLD_KEY @"TT_PERSISTENCE_THRESHOLD"
 
 @interface TTAbstractPersistenceProxy ()
 
-@property (nonatomic, strong, readwrite) NSManagedObjectContext *mainContext;
-@property (nonatomic, strong) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, strong) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-@property (nonatomic, strong) NSManagedObjectContext *diskContext;
-@property (nonatomic, strong) NSURL *storeURL;
-@property (nonatomic) BOOL nestContexts;
+@property(nonatomic, strong, readwrite) NSManagedObjectContext *mainContext;
+@property(nonatomic, strong) NSManagedObjectModel *managedObjectModel;
+@property(nonatomic, strong) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property(nonatomic, strong) NSManagedObjectContext *diskContext;
+@property(nonatomic, strong) NSURL *storeURL;
+@property(nonatomic) BOOL nestContexts;
 
 @property(nonatomic) BOOL requiresDatabaseSeed;
+/**
+ * Prevent asynchronous saving on iOS 5 with nested contexts due to Core Data bug
+ * http://stackoverflow.com/questions/11786436/core-data-nested-managed-object-contexts-and-frequent-deadlocks-freezes/11900851#11900851
+ */
+@property(nonatomic) BOOL allowAsynchronousSaving;
 @end
 
 @implementation TTAbstractPersistenceProxy
 
 - (id)initWithStoreName:(NSString *)storeName nestContexts:(BOOL)nestContexts resetThreshold:(int)resetThreshold
 {
-	self = [super init];
+    self = [super init];
 
-	if (self)
-	{
-		self.storeURL = [[self storeDirectory] URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.sqlite", storeName]];
-		[self checkResetThreshold:resetThreshold];
+    if (self)
+    {
+        self.storeURL = [[self storeDirectory] URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.sqlite", storeName]];
+        [self checkResetThreshold:resetThreshold];
 
-		self.nestContexts = nestContexts;
-	}
+        self.nestContexts = nestContexts;
+        self.allowAsynchronousSaving = TTT_SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"6.0");
+    }
 
-	return self;
+    return self;
 }
 
 /**
@@ -40,103 +48,111 @@
 */
 - (void)checkResetThreshold:(int)resetThreshold
 {
-	if (resetThreshold == 0)
-	{
-		return;
-	}
+    if (resetThreshold == 0)
+    {
+        return;
+    }
 
-	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-	NSString *key = [NSString stringWithFormat:@"%@-%@", TT_PERSISTENCE_THRESHOLD_KEY, self.storeURL.lastPathComponent];
-	BOOL reset = resetThreshold > [defaults integerForKey:key];
-	if (reset && [[NSFileManager defaultManager] fileExistsAtPath:self.storeURL.path])
-	{
-		NSError *error = nil;
-		if ([[NSFileManager defaultManager] removeItemAtURL:self.storeURL error:&error])
-		{
-			ILog(@"Reset store %@", self.storeURL);
-			[defaults setInteger:resetThreshold forKey:key];
-			[defaults synchronize];
-		}
-		else
-		{
-			ELog(@"Could not reset store: %@", error);
-		}
-	}
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *key = [NSString stringWithFormat:@"%@-%@", TT_PERSISTENCE_THRESHOLD_KEY, self.storeURL.lastPathComponent];
+    BOOL reset = resetThreshold > [defaults integerForKey:key];
+    if (reset && [[NSFileManager defaultManager] fileExistsAtPath:self.storeURL.path])
+    {
+        NSError *error = nil;
+        if ([[NSFileManager defaultManager] removeItemAtURL:self.storeURL error:&error])
+        {
+            ILog(@"Reset store %@", self.storeURL);
+            [defaults setInteger:resetThreshold forKey:key];
+            [defaults synchronize];
+        }
+        else
+        {
+            ELog(@"Could not reset store: %@", error);
+        }
+    }
 }
 
 - (NSManagedObjectContext *)newPrivateContext
 {
-	NSManagedObjectContext *context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
-	context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
-	context.parentContext = self.mainContext;
-	context.undoManager = nil;
+    NSManagedObjectContext *context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
+    context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
+    context.undoManager = nil;
 
-	return context;
+    if (self.nestContexts)
+    {
+        context.parentContext = self.mainContext;
+    }
+    else
+    {
+        context.persistentStoreCoordinator = self.persistentStoreCoordinator;
+    }
+
+    return context;
 }
 
 - (NSManagedObjectModel *)managedObjectModel
 {
-	if (!_managedObjectModel)
-	{
-		self.managedObjectModel = [NSManagedObjectModel mergedModelFromBundles:@[[self modelBundle]]];
-	}
+    if (!_managedObjectModel)
+    {
+        self.managedObjectModel = [NSManagedObjectModel mergedModelFromBundles:@[[self modelBundle]]];
+    }
 
-	return _managedObjectModel;
+    return _managedObjectModel;
 }
 
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator
 {
-	if (!_persistentStoreCoordinator)
-	{
-		self.persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
-		if (![self addStoresToPersistentStoreCoordinator:_persistentStoreCoordinator])
-		{
-			self.persistentStoreCoordinator = nil;
-		}
-	}
+    if (!_persistentStoreCoordinator)
+    {
+        self.persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
+        if (![self addStoresToPersistentStoreCoordinator:_persistentStoreCoordinator])
+        {
+            self.persistentStoreCoordinator = nil;
+        }
+    }
 
-	return _persistentStoreCoordinator;
+    return _persistentStoreCoordinator;
 }
 
 - (NSManagedObjectContext *)mainContext
 {
-	if (_mainContext == nil)
-	{
-		self.mainContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
-		_mainContext.mergePolicy = NSOverwriteMergePolicy;
-		_mainContext.undoManager = nil;
+    if (_mainContext == nil)
+    {
+        self.mainContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+        _mainContext.mergePolicy = NSOverwriteMergePolicy;
+        _mainContext.undoManager = nil;
 
-		if (self.nestContexts)
-		{
-			_mainContext.parentContext = self.diskContext;
-		}
-		else
-		{
-			_mainContext.persistentStoreCoordinator = self.persistentStoreCoordinator;
-		}
-        
+        if (self.nestContexts)
+        {
+            _mainContext.parentContext = self.diskContext;
+        }
+        else
+        {
+            _mainContext.persistentStoreCoordinator = self.persistentStoreCoordinator;
+        }
+
         if (self.requiresDatabaseSeed)
         {
             [self seedDatabase];
         }
-	}
+    }
 
-	return _mainContext;
+    return _mainContext;
 }
 
 - (NSManagedObjectContext *)diskContext
 {
-	NSAssert(self.nestContexts, @"Disk context only available when nestContexts is on.");
+    NSAssert(self.nestContexts, @"Disk context only available when nestContexts is on.");
 
-	if (!_diskContext)
-	{
-		self.diskContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
-		_diskContext.mergePolicy = NSOverwriteMergePolicy;
-		_diskContext.undoManager = nil;
-		_diskContext.persistentStoreCoordinator = self.persistentStoreCoordinator;
-	}
+    if (!_diskContext)
+    {
+        self.diskContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
+        _diskContext.mergePolicy = NSOverwriteMergePolicy;
+        _diskContext.undoManager = nil;
+        _diskContext.persistentStoreCoordinator = self.persistentStoreCoordinator;
+    }
 
-	return _diskContext;
+    return _diskContext;
 }
 
 - (void)seedDatabase
@@ -146,95 +162,104 @@
 
 - (void)saveToDisk
 {
-	[self.mainContext performBlockAndWait:^{
-		NSAssert([[NSThread currentThread] isMainThread], @"Must run on main thread");
-		if ([self.mainContext hasChanges])
-		{
-			NSError *error = nil;
-			BOOL saved = [self.mainContext save:&error];
-			if (saved)
-			{
-				DLog(@"Main context saved.");
-			}
-			else
-			{
-				ELog(@"Error saving main context: %@, %@", error, [error userInfo]);
-				return;
-			}
-		}
-		else
-		{
-			ILog(@"Main context has no changes to save to disk");
-		}
+    [self.mainContext performBlockAndWait:^{
+        NSAssert([[NSThread currentThread] isMainThread], @"Must run on main thread");
+        if ([self.mainContext hasChanges])
+        {
+            NSError *error = nil;
+            BOOL saved = [self.mainContext save:&error];
+            if (saved)
+            {
+                DLog(@"Main context saved.");
+            }
+            else
+            {
+                ELog(@"Error saving main context: %@, %@", error, [error userInfo]);
+                return;
+            }
+        }
+        else
+        {
+            ILog(@"Main context has no changes to save to disk");
+        }
 
-		if (self.nestContexts)
-		{
-			[self.diskContext performBlock:^{
-				if ([self.diskContext hasChanges])
-				{
-					NSError *error = nil;
-					BOOL saved = [self.diskContext save:&error];
-					if (saved)
-					{
-						DLog(@"Base context saved.");
-					}
-					else
-					{
-						ELog(@"Error saving base context %@, %@", error, [error userInfo]);
-					}
-				}
-				else
-				{
-					ILog(@"Base context has no changes to save to disk.");
-				}
-			}];
-		}
-	}];
+        if (self.nestContexts)
+        {
+            void (^diskSaveBlock)() = ^{
+                if ([self.diskContext hasChanges])
+                {
+                    NSError *error = nil;
+                    BOOL saved = [self.diskContext save:&error];
+                    if (saved)
+                    {
+                        DLog(@"Base context saved.");
+                    }
+                    else
+                    {
+                        ELog(@"Error saving base context %@, %@", error, [error userInfo]);
+                    }
+                }
+                else
+                {
+                    ILog(@"Base context has no changes to save to disk.");
+                }
+            };
+
+            if (self.allowAsynchronousSaving)
+            {
+                [self.diskContext performBlock:diskSaveBlock];
+            }
+            else
+            {
+                [self.diskContext performBlockAndWait:diskSaveBlock];
+            }
+        }
+    }];
 }
 
 - (BOOL)addStoresToPersistentStoreCoordinator:(NSPersistentStoreCoordinator *)coordinator
 {
-	NSString *configuration = nil;
-	NSError *error = nil;
-	NSPersistentStore *store = nil;
+    NSString *configuration = nil;
+    NSError *error = nil;
+    NSPersistentStore *store = nil;
 
-	if (self.storeURL)
-	{
-		// Add SQLite store
-		NSDictionary *options = @{NSInferMappingModelAutomaticallyOption : @YES, NSMigratePersistentStoresAutomaticallyOption : @YES};
+    if (self.storeURL)
+    {
+        // Add SQLite store
+        NSDictionary *options = @{NSInferMappingModelAutomaticallyOption : @YES, NSMigratePersistentStoresAutomaticallyOption : @YES};
 
         self.requiresDatabaseSeed = ![[NSFileManager defaultManager] fileExistsAtPath:self.storeURL.path];
 
-		store = [coordinator addPersistentStoreWithType:NSSQLiteStoreType
-										  configuration:configuration
-													URL:self.storeURL
-												options:options
-												  error:&error];
-	}
-	else
-	{
-		// Add in-memory store
-		store = [coordinator addPersistentStoreWithType:NSInMemoryStoreType
-										  configuration:configuration
-													URL:nil options:nil error:&error];
-	}
+        store = [coordinator addPersistentStoreWithType:NSSQLiteStoreType
+                                          configuration:configuration
+                                                    URL:self.storeURL
+                                                options:options
+                                                  error:&error];
+    }
+    else
+    {
+        // Add in-memory store
+        store = [coordinator addPersistentStoreWithType:NSInMemoryStoreType
+                                          configuration:configuration
+                                                    URL:nil options:nil error:&error];
+    }
 
-	if (store == nil)
-	{
-		ELog(@"Error adding persistent store: %@", error);
-	}
+    if (store == nil)
+    {
+        ELog(@"Error adding persistent store: %@", error);
+    }
 
-	return store != nil;
+    return store != nil;
 }
 
 - (NSBundle *)modelBundle
 {
-	return [NSBundle mainBundle];
+    return [NSBundle mainBundle];
 }
 
 - (NSURL *)storeDirectory
 {
-	return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
 
 /**
@@ -242,15 +267,15 @@
 */
 - (void)forceReset
 {
-	NSString *key = [NSString stringWithFormat:@"%@-%@", TT_PERSISTENCE_THRESHOLD_KEY, self.storeURL.lastPathComponent];
-	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-	[defaults removeObjectForKey:key];
-	[defaults synchronize];
+    NSString *key = [NSString stringWithFormat:@"%@-%@", TT_PERSISTENCE_THRESHOLD_KEY, self.storeURL.lastPathComponent];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults removeObjectForKey:key];
+    [defaults synchronize];
 
-	if (_diskContext == nil)
-	{
-		[self checkResetThreshold:1];
-	}
+    if (_diskContext == nil)
+    {
+        [self checkResetThreshold:1];
+    }
 }
 
 @end
--- TwelveTwentyToolkit/TwelveTwentyToolkit.h
+++ TwelveTwentyToolkit/TwelveTwentyToolkit.h
@@ -37,3 +37,14 @@ static CFTypeRef CFRetainIfNotNULL (CFTypeRef ref)
     
     return ref;
 }
+
+// http://stackoverflow.com/a/7848772/432782
+// if (TTT_SYSTEM_VERSION_LESS_THAN(@"5.0")) {
+//  ...
+// }
+
+#define TTT_SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define TTT_SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define TTT_SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define TTT_SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define TTT_SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
