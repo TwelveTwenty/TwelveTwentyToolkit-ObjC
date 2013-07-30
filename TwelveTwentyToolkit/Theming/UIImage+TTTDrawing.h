b0bfc6a88f6f4416c921d3d8cf8a7b1e4f02ccd6
Eric-Paul Lecluse <e@epologee.com>
2013-07-30 14:20:04 +0200

    Added image drawing cache.

--- TwelveTwentyToolkit/Theming/UIImage+TTTDrawing.h
+++ TwelveTwentyToolkit/Theming/UIImage+TTTDrawing.h
@@ -5,6 +5,12 @@ typedef void (^TTTDrawingBlock)(CGContextRef ctx, CGRect rect, CGFloat scale);
 
 @interface UIImage (TTTDrawing)
 
++ (void)tttSetDrawingCacheSize:(NSUInteger)bytes;
+
++ (void)tttClearDrawingCache;
+
 + (UIImage *)tttImageWithSize:(CGSize)size drawing:(TTTDrawingBlock)drawing;
+
++ (UIImage *)tttImageWithIdentifier:(NSString *)identifier size:(CGSize)size drawing:(TTTDrawingBlock)drawing;
 
 @end
--- TwelveTwentyToolkit/Theming/UIImage+TTTDrawing.m
+++ TwelveTwentyToolkit/Theming/UIImage+TTTDrawing.m
@@ -1,10 +1,34 @@
+#import <TwelveTwentyToolkit/TwelveTwentyToolkit.h>
 #import "UIImage+TTTDrawing.h"
 
 @implementation UIImage (TTTDrawing)
 
+#define MB * (1024 * 1024)
+
++ (NSCache *)tttDrawingCache
+{
+    static NSCache *cache = nil;
+    static dispatch_once_t predicate;
+    dispatch_once(&predicate, ^{
+        cache = [[NSCache alloc] init];
+        [cache setTotalCostLimit:32 MB];
+    });
+    return cache;
+}
+
++ (void)tttSetDrawingCacheSize:(NSUInteger)bytes
+{
+    [[self tttDrawingCache] setTotalCostLimit:bytes];
+}
+
++ (void)tttClearDrawingCache
+{
+    [[self tttDrawingCache] removeAllObjects];
+}
+
 + (UIImage *)tttImageWithSize:(CGSize)size drawing:(TTTDrawingBlock)drawing
 {
-    CGFloat scale = [UIScreen mainScreen].scale;
+    TTTStaticScreenScale();
     UIGraphicsBeginImageContextWithOptions(size, NO, scale);
     CGContextRef context = UIGraphicsGetCurrentContext();
 
@@ -17,6 +41,21 @@
     }
 
     return nil;
+}
+
++ (UIImage *)tttImageWithIdentifier:(NSString *)identifier size:(CGSize)size drawing:(TTTDrawingBlock)drawing
+{
+    NSString *key = [NSString stringWithFormat:@"%@-%fx%f", identifier, size.width, size.height];
+    UIImage *cachedImage = [[self tttDrawingCache] objectForKey:key];
+
+    if (cachedImage == nil && (cachedImage = [self tttImageWithSize:size drawing:drawing]))
+    {
+        NSUInteger bytesPerPixel = 32;
+        NSUInteger byteCost = (NSUInteger) (size.width * size.height * bytesPerPixel);
+        [[self tttDrawingCache] setObject:cachedImage forKey:key cost:byteCost];
+    }
+
+    return cachedImage;
 }
 
 @end
