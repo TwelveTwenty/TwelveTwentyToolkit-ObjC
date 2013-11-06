2a5089f0f97ca9247d4155832880b0e11448d3fc
Eric-Paul Lecluse <e@epologee.com>
2013-11-06 11:58:35 +0100

    Removed UIKit reference, was added accidentally with AppCode :)

--- TwelveTwentyToolkit/CyclicDelegateRetainer/TTTCyclicDelegateRetainer.h
+++ TwelveTwentyToolkit/CyclicDelegateRetainer/TTTCyclicDelegateRetainer.h
@@ -1,7 +1,6 @@
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 
-@interface TTTCyclicDelegateRetainer : NSObject <UIAlertViewDelegate>
+@interface TTTCyclicDelegateRetainer : NSObject
 
 /// Yes, this delegate is *strongly* referenced. It keeps the original alive until you call `breakRetainCycle`
 @property (nonatomic, strong, readonly) id delegate;
